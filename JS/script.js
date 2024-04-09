e00629f81c311702ead60616f02705290e6adadd
Andressa Vilela <vilelaandressa11@gmail.com>
2024-04-09 17:50:51 -0300

    adicionando novas páginas

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+function clickMenu(){
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+
+}
+
+const viewMoreButtons = document.querySelectorAll('.view-more-expand, .view-more-collapse');
+
+viewMoreButtons.forEach(button => {
+    button.onclick = (event) => {
+        const holder = button.parentElement.parentElement;
+        holder.classList.toggle('view-more--expand');
+    }
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });

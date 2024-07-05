90f658779396285d6fdd6462562c6f8d76d9447f
Andressa Vilela <vilelaandressa11@gmail.com>
2024-07-05 17:09:02 -0300

    ajustes

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,73 @@
-function clickMenu(){
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
-    } else {
-        itens.style.display = 'block'
-    }
+function clickMenu() {
+  if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'block'
+  }
 
 }
 
 const viewMoreButtons = document.querySelectorAll('.view-more-expand, .view-more-collapse');
 
 viewMoreButtons.forEach(button => {
-    button.onclick = (event) => {
-        const holder = button.parentElement.parentElement;
-        holder.classList.toggle('view-more--expand');
-    }
+  button.onclick = (event) => {
+    const holder = button.parentElement.parentElement;
+    holder.classList.toggle('view-more--expand');
+  }
 });
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-  });
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
 
-  
+let swiperCards = new Swiper(".testimonial-content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+let swiperPlans = new Swiper(".plan-content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 4,
+    },
+  },
+});
